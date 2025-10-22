bfc532c509b8abb607601db4fc3f07a4af2fe423
CM Geldenhuys <4649249+CMGeldenhuys@users.noreply.github.com>
2025-10-22 13:12:10 +0200

    feat(utils): add dedicated examplar

--- whalevad/utils.py
+++ whalevad/utils.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import List, Optional
 
 import io
@@ -48,3 +49,41 @@ def load_remote_audio(url: str, filename: str, **kwargs):
             buffer = io.BytesIO(file.read())
             return torchaudio.load(buffer, **kwargs)
 
+
+@dataclass
+class ATBFLExamplar:
+    filename: str
+    start: int
+    end: int
+
+
+ATBFL_REPO_URL = (
+    "https://zenodo.org/records/15092732/files/biodcase_development_set.zip?download=1"
+)
+ATBFL_EXAMPLARS = {
+    "train": ATBFLExamplar(
+        "biodcase_development_set/train/audio/kerguelen2014/2014-06-29T23-00-00_000.wav",
+        0,
+        2500,
+    )
+}
+
+
+def get_atbfl_examplar(
+    filename: Optional[str] = None,
+    start: Optional[int] = None,
+    end: Optional[int] = None,
+    **kwargs,
+):
+    if filename is None:
+        default = ATBFL_EXAMPLARS["train"]
+        filename = default.filename
+        start = start if start is not None else default.start
+        end = end if end is not None else default.end
+    audio, sr = load_remote_audio(ATBFL_REPO_URL, filename, **kwargs)
+    assert sr == 250, f"expected sample rate of ATBFL to be 250Hz got {sr}Hz instead"
+
+    if start is not None or end is not None:
+        audio = audio[:, start:end]
+
+    return audio, sr
